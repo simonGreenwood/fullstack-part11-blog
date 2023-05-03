289847800c93f75fa60dafea9e226f629ab3cedf
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-03 08:03:38 +0100

    deleted some tests

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose")
 const blogRouter = require("./controllers/blog")
 const usersRouter = require("./controllers/users")
 const loginRouter = require("./controllers/login")
+const testingRouter = require("./controllers/testing")
 const config = require("./utils/config")
 const middleware = require("./utils/middleware")
 
@@ -16,4 +17,7 @@ app.use(middleware.userExtractor)
 app.use("/api/login", loginRouter)
 app.use("/api/blogs", blogRouter)
 app.use("/api/users", usersRouter)
+if (process.env.NODE_ENV === "test") {
+  app.use("/api/testing", testingRouter)
+}
 module.exports = app
