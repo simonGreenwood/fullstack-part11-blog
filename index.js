55a0a062e50de7d8b0e0b4dbc715889dd9753a04
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-04 07:33:06 +0100

    testing env variables

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = require("./app")
 const logger = require("./utils/logger")
-const config = require('./utils/config')
+const config = require("./utils/config")
 
 app.listen(config.PORT, () => {
+  console.log(process.env)
   logger.info(`Server running on port ${config.PORT}`)
 })
