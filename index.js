402d3e34e55269ad2bf20079d75e30b3754ff07e
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-04 07:35:30 +0100

    fixed env

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const logger = require("./utils/logger")
 const config = require("./utils/config")
 
 app.listen(config.PORT, () => {
-  console.log(process.env)
   logger.info(`Server running on port ${config.PORT}`)
 })
