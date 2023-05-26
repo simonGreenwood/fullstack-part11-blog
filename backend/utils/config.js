93f589faefd5f7530d0df6fcf722d19675f79cfd
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-26 10:16:21 +0100

    done with 12.21, added mongodb

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,7 +4,11 @@ require("dotenv").config()
 const MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
+    : process.env.NODE_ENV === "development"
+    ? process.env.DEV_MONGODB_URI
     : process.env.MONGODB_URI
 
+console.log(MONGODB_URI)
 const PORT = process.env.PORT || process.env.BACKEND_PORT
 module.exports = { PORT, MONGODB_URI }
+
